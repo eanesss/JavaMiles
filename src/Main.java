76abdd2f8f75806cb7621d658027982e4162dab0
Ekaterina Neshina <bol.2010@yandex.ru>
2022-10-22 15:16:46 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 5034;
+        int oneMile = 20;
+        int miles = amount / oneMile;
+
+        System.out.println("Итого миль:" + miles);
+    }
+}
